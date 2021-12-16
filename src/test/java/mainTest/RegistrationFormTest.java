492b79ae0cdd69c11b19a74070ce958feab5dfd4
Aidyn <adilkhan.a@rahmetapp.kz>
2021-12-16 19:37:25 +0600

    added RegistrationFormTest added Attachments

--- src/test/java/mainTest/RegistrationFormTest.java
+++ src/test/java/mainTest/RegistrationFormTest.java
@@ -32,50 +32,50 @@ public class RegistrationFormTest extends TestBase {
 
     @Test
     void successfulFillFormTest() {
-        step("Open students registration form", () -> {
+        step("Открыть регистрационную форму студента", () -> {
             open("https://demoqa.com/automation-practice-form");
             $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
         });
 
-        step("Fill students registration form", () -> {
-            step("Fill common data", () -> {
+        step("Заполнить регистрационную форму студента", () -> {
+            step("Заполнить основную информацию", () -> {
                 $("#firstName").val(firstName);
                 $("#lastName").val(lastName);
                 $("#userEmail").val(email);
                 $("#genterWrapper").$(byText(gender)).click();
                 $("#userNumber").val(mobile);
             });
-            step("Set date", () -> {
+            step("Установить дату", () -> {
                 $("#dateOfBirthInput").clear();
                 $(".react-datepicker__month-select").selectOption(monthOfBirth);
                 $(".react-datepicker__year-select").selectOption(yearOfBirth);
                 $(".react-datepicker__day--0" + dayOfBirth).click();
             });
-            step("Set subjects", () -> {
+            step("Указать предмет", () -> {
                 $("#subjectsInput").val(subject1);
                 $(".subjects-auto-complete__menu-list").$(byText(subject1)).click();
                 $("#subjectsInput").val(subject2);
                 $(".subjects-auto-complete__menu-list").$(byText(subject2)).click();
             });
-            step("Set hobbies", () -> {
+            step("Указать хобби", () -> {
                 $("#hobbiesWrapper").$(byText(hobby1)).click();
                 $("#hobbiesWrapper").$(byText(hobby2)).click();
                 $("#hobbiesWrapper").$(byText(hobby3)).click();
             });
-            step("Upload image", () ->
+            step("Загрузить картинку", () ->
                     $("#uploadPicture").uploadFromClasspath("img/" + picture));
-            step("Set address", () -> {
+            step("Указать адрес", () -> {
                 $("#currentAddress").val(currentAddress);
                 $("#state").scrollTo().click();
                 $("#stateCity-wrapper").$(byText(state)).click();
                 $("#city").click();
                 $("#stateCity-wrapper").$(byText(city)).click();
             });
-            step("Submit form", () ->
+            step("Подтверждение формы", () ->
                     $("#submit").click());
         });
 
-        step("Verify successful form submit", () -> {
+        step("Проверить что форма заполнена верно", () -> {
             $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
             $x("//td[text()='Student Name']").parent().shouldHave(text(firstName + " " + lastName));
             $x("//td[text()='Student Email']").parent().shouldHave(text(email));
